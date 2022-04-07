41fda8dda3a3385e2e02159807b0e162338003f7
Kayode Oladipo <khaox07@gmail.com>
2022-04-07 08:53:16 +0100

    Added len, and total_price feature

--- cart/cart.py
+++ cart/cart.py
@@ -15,6 +15,12 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(product['quantity'] for product in self.cart.values())
+
     def add(self, product, quantity=1, action=None):
         """
         Add a product to the cart or update its quantity.
@@ -79,6 +85,19 @@ class Cart(object):
                 break
             else:
                 print("Something Wrong")
+    
+    def get_total_price(self):
+        """
+        Get total price of all products in cart
+        """
+        return str(sum(Decimal(product['price']) * product['quantity'] for product in self.cart.values()))
+    
+
+    def is_empty(self):
+        """
+        Check if cart is empty
+        """
+        return True if len(self.cart) == 0 else False
 
     def clear(self):
         # empty cart
